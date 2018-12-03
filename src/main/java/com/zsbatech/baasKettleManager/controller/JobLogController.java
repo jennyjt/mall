acc997a73d79658a111bd2ff2f959b80a4de45ef
wuhl <wuhaoli@zsbatech.com>
2018-12-03 17:12:28 +0800

    modify log query method

--- src/main/java/com/zsbatech/baasKettleManager/controller/JobLogController.java
+++ src/main/java/com/zsbatech/baasKettleManager/controller/JobLogController.java
@@ -1,12 +1,14 @@
 package com.zsbatech.baasKettleManager.controller;
 
 import com.zsbatech.baasKettleManager.model.JobLog;
+import com.zsbatech.baasKettleManager.service.JobLogService;
 import com.zsbatech.base.common.ResponseData;
 import com.zsbatech.base.constants.RequestField;
 import com.zsbatech.base.constants.Response;
 import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +25,9 @@ public class JobLogController {
 
     private static Logger logger = LoggerFactory.getLogger(JobLogController.class);
 
+    @Autowired
+    private JobLogService jobLogService;
+
     @ApiOperation(value = "查询日志信息", notes = "", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ApiResponses({@ApiResponse(code = Response.OK, message = "查询成功"),})
     @ApiImplicitParams(
@@ -30,18 +35,21 @@ public class JobLogController {
                     @ApiImplicitParam(paramType = "header", name = RequestField.TOKEN, dataType = "String", required = true, value = "token"),
                     @ApiImplicitParam(paramType = "query", name = "job_id", dataType = "Integer", required = false, value = "任务id"),
                     @ApiImplicitParam(paramType = "query", name = "user_id", dataType = "String", required = false, value = "用户id"),
-                    @ApiImplicitParam(paramType = "query", name = "job_date", dataType = "String", required = false, value = "任务执行日期")
+                    @ApiImplicitParam(paramType = "query", name = "job_date_from", dataType = "String", required = false, value = "查询起始日期"),
+                    @ApiImplicitParam(paramType = "query", name = "job_date_end", dataType = "String", required = false, value = "查询结束日期")
             }
     )
     @RequestMapping(value = "/query", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public ResponseData<List<JobLog>> fileUpload(HttpServletRequest request,
+    public ResponseData<List<JobLog>> queryLogInfo(HttpServletRequest request,
                                                  @RequestParam(name = "job_id", required = false) Integer jobId,
                                                  @RequestParam(name = "user_id", required = false) String createUser,
-                                                 @RequestParam(name = "job_date", required = false) String executeDate) {
+                                                 @RequestParam(name = "job_date_from", required = false) String jobDateFrom,
+                                                 @RequestParam(name = "job_date_end", required = false) String jobDateEnd) {
         ResponseData<List<JobLog>> responseData = new ResponseData<>();
         //UniToken uniToken = JWTUtils.validateToken(request);
-
+        List<JobLog> jobLogList = jobLogService.getJobLogInfo(jobId, createUser, jobDateFrom, jobDateEnd);
+        responseData.setResult(jobLogList);
         return responseData;
     }
 }
